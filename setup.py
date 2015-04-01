355afa0aa6ba39109e14c072e3ab74b6aa69067b
Richard Neumann <r.neumann@homeinfo.de>
2015-04-01 12:50:09 +0200

    dito

---
+++ setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='terminallib',
+    version='1.0.0',
+    author='Richard Neumann',
+    author_email='r.neumann@homeinfo.de',
+    packages=['terminallib'],
+    requires=['peewee'],
+    license=open('LICENSE.txt').read(),
+    description=('Homeinfo Terminal Libary')
+    )
