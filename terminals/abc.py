82121f990aa0fe286f82cdf135b6c259e3021173
Richard Neumann <r.neumann@homeinfo.de>
2015-04-22 08:43:10 +0200

    Re-added abc module

---
+++ terminals/abc.py
@@ -0,0 +1,28 @@
+"""Abstract base classes for the terminal management"""
+
+__author__ = 'Richard Neumann <r.neumann@homeinfo.de>'
+__date__ = '22.04.2015'
+__all__ = ['TerminalAware']
+
+
+class TerminalAware():
+    """Manages terminals"""
+
+    def __init__(self, terminal):
+        """Sets user name and password"""
+        self.terminal = terminal
+
+    @property
+    def cid(self):
+        """Returns the customer identifier"""
+        return self.terminal.cid
+
+    @property
+    def tid(self):
+        """Returns the terminal identifier"""
+        return self.terminal.tid
+
+    @property
+    def idstr(self):
+        """Returns a unique string identifier"""
+        return '.'.join([str(self.tid), str(self.cid)])
--- terminals/db.py
+++ terminals/db.py
@@ -37,29 +37,6 @@ class TermgrModel(Model):
     """The table's primary key"""
 
 
-class TerminalAware():
-    """Manages terminals"""
-
-    def __init__(self, terminal):
-        """Sets user name and password"""
-        self.terminal = terminal
-
-    @property
-    def cid(self):
-        """Returns the customer identifier"""
-        return self.terminal.cid
-
-    @property
-    def tid(self):
-        """Returns the terminal identifier"""
-        return self.terminal.tid
-
-    @property
-    def idstr(self):
-        """Returns a unique string identifier"""
-        return '.'.join([str(self.tid), str(self.cid)])
-
-
 @create
 class Class(TermgrModel):
     """Terminal classes"""
