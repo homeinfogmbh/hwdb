d0a3486491e18b9100c8acee0bd28b159f7da32e
Richard Neumann <r.neumann@homeinfo.de>
2015-04-22 10:29:05 +0200

    fix

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
                 ('/usr/local/sbin',
                  ['files/usr/local/sbin/build-key-terminal',
                   'files/usr/local/sbin/hosts.gen',
+                  'files/usr/local/sbin/monitoring-clients.gen',
                   'files/usr/local/sbin/openvpn-clients.gen']),
                 ('/usr/local/share/terminals',
                  ['files/usr/local/share/terminals/pacman.conf.temp',
