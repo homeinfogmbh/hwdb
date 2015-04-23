51c5bbc074199bb483e2ebe5095fdf5dde51681d
Richard Neumann <r.neumann@homeinfo.de>
2015-04-23 14:05:03 +0200

    Added config generation to DB model

--- terminals/db.py
+++ terminals/db.py
@@ -318,6 +318,35 @@ class Terminal(TermgrModel):
         return chain(AdministratorTerminals.operators(self),
                      Administrator.root)
 
+    def appconf(self, trackingid=42, checkdate=False):
+        """Generates the content for the config.ini, respectively
+        the application.conf configuration file for the application
+        """
+        if self.class_.touch:
+            mouse_visible = False
+        else:
+            mouse_visible = True
+        if self.rotation is None:
+            rotation = True
+            rotation_degrees = 0
+        else:
+            rot = int(self.rotation)
+            if rot == 0:
+                rotation = False
+                rotation_degrees = 0
+            else:
+                rotation = True
+                rotation_degrees = rot
+        knr = '='.join(['knr', str(self.customer.id)])
+        monitorid = '='.join(['monitorid', str(self.virtual_display)])
+        trackingid = '='.join(['trackingid', str(trackingid)])
+        mouse_visible = '='.join(['mouse_visible', str(mouse_visible).lower()])
+        checkdate = '='.join(['checkdate', str(checkdate).lower()])
+        rotation = '='.join(['rotation', str(rotation).lower()])
+        rotation_degrees = '='.join(['rotationDegrees', str(rotation_degrees)])
+        return '\n'.join([knr, monitorid, trackingid, mouse_visible,
+                          checkdate, rotation, rotation_degrees])
+
     def gen_vpn_keys(self):
         """Generates an OpenVPN key pair for the terminal"""
         build_script = openvpn['BUILD_SCRIPT']
