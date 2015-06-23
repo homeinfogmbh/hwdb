6ecebc60c40939e3209ea7fc85f870717261a2d1
Richard Neumann <r.neumann@homeinfo.de>
2015-06-23 14:17:17 +0200

    Added RemoteController

--- terminals/config.py
+++ terminals/config.py
@@ -1,12 +1,27 @@
 """Terminal setup configuration"""
 
-from configparser import ConfigParser
+from homeinfo.lib.config import Configuration
 
-__all__ = ['db', 'net']
+__all__ = ['terminals_config']
 
-CONFIG_FILE = '/etc/terminals.conf'
-config = ConfigParser()
-config.read(CONFIG_FILE)
 
-db = config['db']
-net = config['net']
+class TerminalsConfiguration(Configuration):
+    """Main terminals configuration class"""
+
+    @property
+    def db(self):
+        self.load()
+        return self['db']
+
+    @property
+    def net(self):
+        self.load()
+        return self['net']
+
+    @property
+    def ssh(self):
+        self.load()
+        return self['ssh']
+
+
+terminals_config = TerminalsConfiguration('/etc/terminals.conf')
---
+++ terminals/ctrl.py
@@ -0,0 +1,123 @@
+"""Library for terminal remote control"""
+
+from os.path import join
+from tempfile import NamedTemporaryFile
+from itertools import chain
+
+from homeinfo.lib.system import run, ProcessResult
+from homeinfo.terminals.abc import TerminalAware
+
+from .config import terminals_config
+
+__all__ = ['RemoteController']
+
+
+class RemoteController(TerminalAware):
+    """Controls a terminal remotely"""
+
+    def __init__(self, user, terminal, keyfile=None, white_list=None, bl=None):
+        """Initializes a remote terminal controller"""
+        super().__init__(terminal)
+        self._user = user
+        self._keyfile = keyfile
+        # Commands white and black list
+        self._white_list = white_list
+        self._black_list = bl
+        # FUrther options for SSH
+        _SSH_OPTS = {
+            # Trick SSH it into not checking the host key
+            'UserKnownHostsFile':
+                terminals_config.ssh['USER_KNOWN_HOSTS_FILE'],
+            'StrictHostKeyChecking':
+                terminals_config.ssh['STRICT_HOST_KEY_CHECKING'],
+            # Set timeout to avoid blocking of rsync / ssh call
+            'ConnectTimeout': terminals_config.ssh['CONNECT_TIMEOUT']}
+
+    @property
+    def user(self):
+        """Returns the user name"""
+        return self._user
+
+    @property
+    def keyfile(self):
+        """Returns the path to the SSH key file"""
+        return self._keyfile or join(
+            '', 'home', self.user, '.ssh', 'terminals')
+
+    @property
+    def _identity(self):
+        """Returns the SSH identity file argument
+        with the respective identity file's path
+        """
+        return ' '.join(['-i', self.keyfile])
+
+    @property
+    def _ssh_opts(self):
+        """Returns options for SSH"""
+        return ' '.join([
+            ' '.join(['-o', '='.join([key, self._SSH_OPTS[key]])])
+            for key in self._SSH_OPTS])
+
+    @property
+    def _ssh_cmd(self):
+        """Returns the SSH basic command line"""
+        return ' '.join([terminals_config.ssh['SSH_BIN'], self._identity,
+                         self._ssh_options])
+
+    @property
+    def _rsync_shell(self):
+        """Returns the rsync remote shell"""
+        return ' '.join(['-e', ''.join(['"', self._ssh_cmd, '"'])])
+
+    @property
+    def _user_host(self):
+        """Returns the respective user@host string"""
+        return '@'.join([self.user, str(self.terminal.ipv4addr)])
+
+    def _remote(self, cmd, *args):
+        """Makes a command remote"""
+        return ' '.join(chain([self._ssh_cmd, self._user_host, cmd], args))
+
+    def _remote_file(self, src):
+        """Returns a remote file path"""
+        return ':'.join([self._user_host, src])
+
+    def _rsync(self, src, dst):
+        """Returns an rsync command line to retrieve
+        src file from terminal to local file dst
+        """
+        return ' '.join([terminals_config.ssh['RSYNC_BIN'],
+                         self._remote_shell, src, dst])
+
+    def _check_command(self, cmd):
+        """Checks the command against the white- and blacklists"""
+        if self._white_list is not None:
+            if cmd not in self._white_list:
+                return False
+        if self._black_list is not None:
+            if cmd in self._black_list:
+                return False
+        return True
+
+    def execute(self, cmd, *args):
+        """Executes a certain command on a remote terminal"""
+        if self._check_command(cmd):
+            return run(self._remote(cmd, *args), shell=True)
+        else:
+            return ProcessResult(3, stderr='Command not allowed.'.encode())
+
+    def getfile(self, file):
+        """Gets a file from a remote terminal"""
+        with NamedTemporaryFile('rb') as tmp:
+            rsync = self._rsync(self._remote_file(file), tmp.name)
+            pr = run(rsync, shell=True)
+            if pr:
+                return tmp.read()
+            else:
+                return pr
+
+    def sendfile(self, src, dst):
+        """Gets a file from a remote terminal"""
+        rsync = self._rsync(src, self._remote_file(dst))
+        pr = run(rsync, shell=True)
+        return pr
