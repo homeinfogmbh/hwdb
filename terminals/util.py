2d03dac836e8150b066fd3271c92ea19cf2babac
Richard Neumann <r.neumann@homeinfo.de>
2016-07-26 18:00:41 +0200

    Outsourced termutil into library classes

--- terminals/util.py
+++ terminals/util.py
@@ -1,9 +1,19 @@
 """Terminal query utilities"""
 
+from sys import stderr
+
 from homeinfo.terminals.filter import TerminalFilter
 from homeinfo.terminals.orm import Class, Domain, OS, Terminal,\
     AddressUnconfiguredError
 
+
+__all__ = [
+    'DeploymentFilter',
+    'TestingFilter',
+    'TerminalUtil',
+    'ClassUtil',
+    'OSUtil',
+    'DomainUtil']
 
 class DeploymentFilter():
     """Filters terminals for their deployment state"""
@@ -50,30 +60,84 @@ class TerminalUtil():
             '{ipv4addr: >12.12} {deployed: >21.21} {testing: >6.6}  '
             '{address: <50.50} {annotation}')
 
-    def __init__(self, *args,
+    def __init__(self, expr,
                  deployed=None, undeployed=None,
                  testing=None, productive=None):
-        self.args = args
+        self.expr = expr
         self.deployed = deployed
         self.undeployed = undeployed
         self.testing = testing
         self.productive = productive
 
-    def __iter__(self):
+    def __str__(self):
         """Filters terminals for productiveness and deployment"""
-        for arg in self.args:
-            # TODO: Implement
-            raise NotImplementedError()
+        for terminal in self.filter(terminals):
+            try:
+                address_str = str(terminal.address)
+            except AddressUnconfiguredError:
+                address_str = 'N/A'
+
+            print(template.format(
+                id=str(terminal.id),
+                tid=str(terminal.tid),
+                cid=str(terminal.customer.id),
+                vid=str(terminal.vid),
+                ipv4addr=str(terminal.ipv4addr),
+                deployed=str(terminal.deployed),
+                testing=str(terminal.testing),
+                address=address_str,
+                annotation=str(terminal.annotation)))
 
     def __str__(self):
         """Prints filtered terminals"""
-        return '\n'.join((self.format(terminal) for terminal in self))
+        return '\n'.join(self.format(terminal) for terminal in self)
+
+    def __iter__(self):
+        """Filters the terminals by the respective settings"""
+        for terminal in self.terminals:
+            if self.deployed:
+                if self.undeployed:
+                    if self.testing:
+                        if self.productive:
+                            yield terminal
+                        else:
+                            if terminal.testing:
+                                yield terminal
+                    else:
+                        if self.productive:
+                            if not terminal.testing:
+                                yield terminal
+                else:
+                    if terminal.deployed is not None:
+                        if self.testing:
+                            if self.productive:
+                                yield terminal
+                            else:
+                                if terminal.testing:
+                                    yield terminal
+                        else:
+                            if self.productive:
+                                if not terminal.testing:
+                                    yield terminal
+            else:
+                if terminal.deployed is None:
+                    if self.undeployed:
+                        if self.testing:
+                            if self.productive:
+                                yield terminal
+                            else:
+                                if terminal.testing:
+                                    yield terminal
+                        else:
+                            if self.productive:
+                                if not terminal.testing:
+                                    yield terminal
 
     @property
     def terminals(self):
         """Yields terminals selected by self.expr"""
         if self.expr:
-            return TerminalFilter(expr)
+            return TerminalFilter(self.expr)
         else:
             return Terminal.select().where(True)
 
@@ -94,6 +158,54 @@ class TerminalUtil():
             testing=str(terminal.testing),
             address=address_str,
             annotation=str(terminal.annotation))
+
+    @classmethod
+    def find(cls, street, house_number, annotation=None):
+        """Finds terminals bathing the specified location"""
+
+        for terminal in Terminal.select().where(
+                ~ (Terminal.location >> None)):
+            address = terminal.location.address
+            house_number_ = address.house_number.replace(' ', '')
+            annotation_ = terminal.location.annotation
+
+            if street.lower() in address.street.lower():
+                if house_number.lower() == house_number_.lower():
+                    if annotation is not None:
+                        if (annotation.lower() in annotation_.lower()):
+                            yield terminal
+                    else:
+                        yield terminal
+
+    @classmethod
+    def get(cls, street, house_number, annotation=None, index=None):
+        """Finds a terminal by its location"""
+
+        def _print(terminal):
+            print(repr(terminal.location), file=stderr)
+            print(str(terminal))
+            return True
+
+        terminals = [t for t in cls.find(
+            street, house_number, annotation=None)]
+
+        if not terminals:
+            print('No terminal matching query.', file=stderr)
+        elif len(terminals) == 1:
+            terminal = terminals[0]
+            return _print(terminal)
+        elif index is not None:
+            try:
+                terminal = terminals[index]
+            except IndexError:
+                print('No {index}th terminal available ({n}).'.format(
+                    index=index, n=len(terminals)), file=stderr)
+                return False
+            else:
+                return _print(terminal)
+        else:
+            print('Ambiguous terminals:', terminals, file=stderr)
+            return False
 
 
 class ClassUtil():
