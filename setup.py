dfb5eaf82fe53433339f6ad1236aba630c3d4bee
Richard Neumann <r.neumann@homeinfo.de>
2016-09-07 12:12:11 +0200

    Cleanup

--- setup.py
+++ setup.py
@@ -14,13 +14,17 @@ setup(
     packages=['homeinfo.terminals'],
     data_files=[
         # Main configuration
-        ('/etc', ['files/etc/terminals.conf']),
-        # Miscellaneous scripts
-        ('/usr/lib/terminals',
-         ['files/usr/lib/terminals/bindcfg-gen',
-          'files/usr/lib/terminals/chkstats',
-          'files/usr/lib/terminals/chksync',
-          'files/usr/lib/terminals/openvpncfg-gen']),
-        # Utilty script
-        ('/usr/bin', ['files/usr/bin/termutil'])],
+        ('/etc', ['files/terminals.conf']),
+        # Template files
+        ('/usr/share/terminals',
+         ['files/bind.zone.temp',
+          'files/openvpn.conf.temp',
+          'files/pacman.conf.temp']),
+        # Executables
+        ('/usr/bin',
+         ['files/termutil',
+          'files/bindcfg-gen',
+          'files/chkstats',
+          'files/chksync',
+          'files/openvpncfg-gen'])],
     description=("HOMEINFO's terminal libary"))
