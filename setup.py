038358dcbcca9133cbc1ebac7a63afb08a892be7
Richard Neumann <r.neumann@homeinfo.de>
2017-10-06 12:39:59 +0200

    Setup refac.

--- setup.py
+++ setup.py
@@ -5,25 +5,24 @@ from distutils.core import setup
 setup(
     name='homeinfo.terminals',
     version='latest',
-    author='Richard Neumann',
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='info@homeinfo.de',
+    maintainer='Richard Neumann',
+    maintainer_email='r.neumann@homeinfo.de',
     package_dir={'homeinfo': ''},
     packages=['homeinfo.terminals'],
+    scripts=[
+        'files/termutil',
+        'files/chkstats',
+        'files/chksync',
+        'files/refresh-termstats',
+        'files/bindcfg-gen',
+        'files/openvpncfg-gen'],
     data_files=[
-        # Template files
         ('/usr/share/terminals',
          ['files/openvpn.conf.temp',
           'files/pacman.conf.temp']),
-        # Executables
-        ('/usr/local/bin',
-         ['files/termutil',
-          'files/chkstats',
-          'files/chksync',
-          'files/refresh-termstats']),
-        ('/usr/local/sbin',
-         ['files/bindcfg-gen',
-          'files/openvpncfg-gen']),
-        # Systemd units
         ('/usr/lib/systemd/system',
          ['files/refresh-termstats.service',
           'files/refresh-termstats.timer'])],
-    description=("HOMEINFO's terminal libary"))
+    description=("HOMEINFO's terminal libary."))
