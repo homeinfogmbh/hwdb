be38fbdc641e5fe1f6e2fd30ced71c911ea9d760
Richard Neumann <r.neumann@homeinfo.de>
2019-02-08 15:09:42 +0100

    Fix.

--- backend/terminallib/config.py
+++ backend/terminallib/config.py
@@ -1,6 +1,6 @@
 """Terminal setup configuration."""
 
-from ipaddress import IPv4Network
+from ipaddress import IPv4Address, IPv4Network
 
 from configlib import loadcfg
 
@@ -10,4 +10,4 @@ __all__ = ['CONFIG', 'WIREGUARD_NETWORK']
 
 CONFIG = loadcfg('terminals.conf')
 WIREGUARD_NETWORK = IPv4Network(CONFIG['wireguard']['network'])
-WIREGUARD_SERVER = IPv4Network(CONFIG['wireguard']['server'])
+WIREGUARD_SERVER = IPv4Address(CONFIG['wireguard']['server'])
