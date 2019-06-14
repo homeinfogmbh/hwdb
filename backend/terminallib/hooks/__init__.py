c9ab7201eefeb45a2526f6c60339353f6f3a1b42
Richard Neumann <r.neumann@homeinfo.de>
2019-06-14 13:29:21 +0200

    Refac.

--- backend/terminallib/hooks/__init__.py
+++ backend/terminallib/hooks/__init__.py
@@ -5,13 +5,4 @@ from terminallib.hooks.nagios import nagioscfgen
 from terminallib.hooks.openvpn import openvpncfgen
 
 
-__all__ = ['bind9cfgen', 'nagioscfgen', 'openvpncfgen', 'run']
-
-
-def run():
-    """Runs all hooks."""
-
-    success_bind9 = bind9cfgen()
-    success_nagios = nagioscfgen()
-    success_openvpn = openvpncfgen()
-    return success_bind9 and success_nagios and success_openvpn
+__all__ = ['bind9cfgen', 'nagioscfgen', 'openvpncfgen']
