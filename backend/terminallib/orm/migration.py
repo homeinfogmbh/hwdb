0ebd852038ba4b89febee3b66246dc5f8f65b28d
Richard Neumann <r.neumann@homeinfo.de>
2020-01-30 12:47:21 +0100

    Migrating to new IPv4AddressField.

--- backend/terminallib/orm/migration.py
+++ backend/terminallib/orm/migration.py
@@ -1,26 +1,11 @@
-"""Common ORM models."""
+"""IP-related fields."""
 
 from ipaddress import IPv4Address
 
 from peewee import BigIntegerField
 
-from peeweeplus import JSONModel, MySQLDatabase
 
-from terminallib.config import CONFIG
-
-
-__all__ = ['DATABASE', 'BaseModel', 'LegacyIPv4AddressField']
-
-
-DATABASE = MySQLDatabase.from_config(CONFIG['Database'])
-
-
-class BaseModel(JSONModel):
-    """Terminal manager basic Model."""
-
-    class Meta:     # pylint: disable=C0111,R0903
-        database = DATABASE
-        schema = database.database
+__all__ = ['LegacyIPv4AddressField']
 
 
 class LegacyIPv4AddressField(BigIntegerField):
