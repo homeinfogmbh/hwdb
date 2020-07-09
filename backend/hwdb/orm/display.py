d5cbc3ad018949426541aeb95047a77f1e9b1e47
Richard Neumann <r.neumann@homeinfo.de>
2020-07-09 12:58:22 +0200

    Changed fields.

--- backend/hwdb/orm/display.py
+++ backend/hwdb/orm/display.py
@@ -1,29 +1,28 @@
 """Digital signage displays."""
 
-from peewee import BooleanField
 from peewee import CharField
 from peewee import DateTimeField
 from peewee import ForeignKeyField
 
 from hwdb.orm.common import BaseModel
-from hwdb.orm.deployment import Deployment
 from hwdb.orm.system import System
+from mdb import Address
 
 
 __all__ = ['Display']
 
 
-class Display(BaseModel):
+class Display(BaseModel):   # pylint: disable=R0903
     """A physical display out in the field."""
 
-    deployment = ForeignKeyField(
-        Deployment, null=True, column_name='deployment', backref='displays',
+    address = ForeignKeyField(
+        Address, null=True, column_name='address', backref='displays',
         on_delete='SET NULL', on_update='CASCADE')
+    annotation = CharField(255, null=True)
     system = ForeignKeyField(
         System, null=True, column_name='system', backref='displays',
         on_delete='SET NULL', on_update='CASCADE')
     installed = DateTimeField(null=True)
-    monitor = BooleanField(null=True)
     make = CharField(255, null=True)   # Hardware make.
     model = CharField(255, null=True)   # Hardware model.
     serial_number = CharField(255, null=True)
