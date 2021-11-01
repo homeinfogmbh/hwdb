f76cbea0d0094114ce5afabb7ead0b71458a8ccb
Richard Neumann <mail@richard-neumann.de>
2021-11-01 19:20:51 +0100

    Add IPSocket

--- backend/hwdb/types.py
+++ backend/hwdb/types.py
@@ -1,12 +1,25 @@
 """Common types."""
 
 from ipaddress import IPv4Address, IPv6Address, IPv4Network, IPv6Network
-from typing import Iterable, Union
+from typing import Iterable, NamedTuple, Union
 
 
-__all__ = ['IPAddress', 'IPNetwork', 'IPAddresses']
+__all__ = ['IPAddress', 'IPNetwork', 'IPAddresses', 'IPSocket']
 
 
 IPAddress = Union[IPv4Address, IPv6Address]
 IPNetwork = Union[IPv4Network, IPv6Network]
 IPAddresses = Iterable[IPAddress]
+
+
+class IPSocket(NamedTuple):
+    """Represents an IP socket."""
+
+    ipaddress: IPAddress
+    port: int
+
+    def __str__(self):
+        if isinstance(self.ipaddress, IPv6Address):
+            return f'[{self.ipaddress}]:{self.port}'
+
+        return f'{self.ipaddress}:{self.port}'
